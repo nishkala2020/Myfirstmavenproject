3490692d905db40638c5a6dcf03fb0f6f4972acd
nishkala2020 <nishkala2810@gmail.com>
2021-04-21 16:46:08 -0400

    created my first project

---
+++ src/main/java/com/nisha/AddTwoNumbers.java
@@ -0,0 +1,12 @@
+package com.nisha;
+
+public class AddTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int num1 = 90, num2 = 15, sum;
+        sum = num1 + num2;
+
+        System.out.println("Sum of these numbers: "+sum);
+    }
+}
